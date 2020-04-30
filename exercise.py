de901cc92ea17df3e7d96ccacf7915a79f397380
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-04-30 13:55:11 -0500

    finished part four of tracking student exercises practice

--- exercise.py
+++ exercise.py
@@ -3,3 +3,6 @@ class Exercise:
     def __init__(self, name, language):
         self.name = name
         self.language = language
+
+    def __repr__(self):
+        return f'The {self.name} exercise is in {self.language}'
--- student.py
+++ student.py
@@ -1,19 +1,32 @@
-from person import Person
+class Student():
 
-class Student(Person):
+    def __init__(self, first, last, slack, cohort):
+        self.first_name = first
+        self.last_name = last
+        self.slack = slack
+        self.cohort = cohort
 
-    def __init__(self, first_name, last_name, slack_handle):
-        super().__init__(first_name, last_name, slack_handle)
-        self.cohort = ""
-        self.exercises = []
+    def __repr__(self):
+        return f'{self.first_name} {self.last_name} is in {self.cohort}'
 
-    def exercise_report(self):
-        exercises = []
-        for exercise in self.exercises:
-            exercises.append(exercise.name)
-        last_exercise = exercises[-1]
-        exercises.pop()
-        joined_exercises = ", ".join(exercises)
-        print(f"{self.first_name} is working on {joined_exercises} and {last_exercise}")
+
+
+# from person import Person
+
+# class Student(Person):
+
+#     def __init__(self, first_name, last_name, slack_handle):
+#         super().__init__(first_name, last_name, slack_handle)
+#         self.cohort = ""
+#         self.exercises = []
+
+#     def exercise_report(self):
+#         exercises = []
+#         for exercise in self.exercises:
+#             exercises.append(exercise.name)
+#         last_exercise = exercises[-1]
+#         exercises.pop()
+#         joined_exercises = ", ".join(exercises)
+#         print(f"{self.first_name} is working on {joined_exercises} and {last_exercise}")
 
 
